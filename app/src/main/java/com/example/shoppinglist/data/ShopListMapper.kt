07cd4699e5eced6b08c5da42c92988003fdba786
Artem <artem.mamedov5375@gmail.com>
2023-12-28 17:39:51 +0200

    Final

---
+++ app/src/main/java/com/example/shoppinglist/data/ShopListMapper.kt
@@ -0,0 +1,23 @@
+package com.example.shoppinglist.data
+
+import com.example.shoppinglist.domain.ShopItem
+
+class ShopListMapper {
+
+    fun mapEntityToDbModel(shopItem: ShopItem): ShopItemDbModel = ShopItemDbModel(
+        id = shopItem.id,
+        name = shopItem.name,
+        amount = shopItem.amount,
+        enabled = shopItem.enabled
+    )
+    fun mapDbModelToEntity(shopItemDbModel: ShopItemDbModel): ShopItem = ShopItem(
+        id = shopItemDbModel.id,
+        name = shopItemDbModel.name,
+        amount = shopItemDbModel.amount,
+        enabled = shopItemDbModel.enabled
+    )
+
+    fun mapListDbModelToListEntity(list: List<ShopItemDbModel>) = list.map {
+        mapDbModelToEntity(it)
+    }
+}
--- app/src/main/java/com/example/shoppinglist/presentation/ShopItemViewModel.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopItemViewModel.kt
@@ -1,17 +1,24 @@
 package com.example.shoppinglist.presentation
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.shoppinglist.data.ShopListRepositoryImpl
 import com.example.shoppinglist.domain.AddShopItemUseCase
 import com.example.shoppinglist.domain.EditShopItemUseCase
 import com.example.shoppinglist.domain.GetShopItemUseCase
 import com.example.shoppinglist.domain.ShopItem
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 
-class ShopItemViewModel : ViewModel() {
+class ShopItemViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val repository = ShopListRepositoryImpl
+    private val repository = ShopListRepositoryImpl(application)
 
     private val getShopItemUseCase = GetShopItemUseCase(repository)
     private val addShopItemUseCase = AddShopItemUseCase(repository)
@@ -34,8 +41,10 @@ class ShopItemViewModel : ViewModel() {
         get() = _shouldCloseScreen
 
     fun getShopItem(shopItemId: Int) {
-        val item = getShopItemUseCase.getShopItem(shopItemId)
-        _shopItem.value = item
+        viewModelScope.launch {
+            val item = getShopItemUseCase.getShopItem(shopItemId)
+            _shopItem.value = item
+        }
     }
 
     fun addShopItem(inputName: String?, inputAmount: String?) {
@@ -43,9 +52,11 @@ class ShopItemViewModel : ViewModel() {
         val amount = parseAmount(inputAmount)
         val fieldsValid = validateInput(name, amount)
         if (fieldsValid) {
-            val shopItem = ShopItem(name, amount, true)
-            addShopItemUseCase.addShopItem(shopItem)
-            finishWork()
+            viewModelScope.launch {
+                val shopItem = ShopItem(name, amount, true)
+                addShopItemUseCase.addShopItem(shopItem)
+                finishWork()
+            }
         }
     }
 
@@ -55,9 +66,11 @@ class ShopItemViewModel : ViewModel() {
         val fieldsValid = validateInput(name, amount)
         if (fieldsValid) {
             _shopItem.value?.let {
-                val item = it.copy(name = name, amount = amount)
-                editShopItemUseCase.editShopItem(item)
-                finishWork()
+                viewModelScope.launch {
+                    val item = it.copy(name = name, amount = amount)
+                    editShopItemUseCase.editShopItem(item)
+                    finishWork()
+                }
             }
         }
     }
