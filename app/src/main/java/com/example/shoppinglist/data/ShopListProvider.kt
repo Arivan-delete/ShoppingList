0a9ed08467216917005854f287e046e429a92f56
Artem <artem.mamedov5375@gmail.com>
2024-01-04 20:39:51 +0200

    Content Provider

---
+++ app/src/main/java/com/example/shoppinglist/data/ShopListProvider.kt
@@ -0,0 +1,107 @@
+package com.example.shoppinglist.data
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.content.UriMatcher
+import android.database.Cursor
+import android.net.Uri
+import android.util.Log
+import com.example.shoppinglist.domain.ShopItem
+import com.example.shoppinglist.presentation.ShopApplication
+import javax.inject.Inject
+
+class ShopListProvider: ContentProvider() {
+
+    private val component by lazy {
+        (context as ShopApplication).component
+    }
+
+    @Inject
+    lateinit var shopListDao: ShopListDao
+
+    @Inject
+    lateinit var mapper: ShopListMapper
+
+    private val uriMatcher = UriMatcher(UriMatcher.NO_MATCH).apply {
+        addURI("com.example.shoppinglist", "shop_items", GET_SHOP_ITEMS_QUERY)
+        addURI("com.example.shoppinglist", "shop_items/#", GET_SHOP_ITEM_BY_ID_QUERY)
+    }
+    override fun onCreate(): Boolean {
+        component.inject(this)
+        return true
+    }
+
+    override fun query(
+        p0: Uri,
+        p1: Array<out String>?,
+        p2: String?,
+        p3: Array<out String>?,
+        p4: String?
+    ): Cursor? {
+        return when(uriMatcher.match(p0)) {
+            GET_SHOP_ITEMS_QUERY -> {
+                shopListDao.getShopListCursor()
+            }
+            else -> null
+        }
+    }
+
+    override fun getType(p0: Uri): String? {
+        TODO("Not yet implemented")
+    }
+
+    override fun insert(p0: Uri, p1: ContentValues?): Uri? {
+        when(uriMatcher.match(p0)) {
+            GET_SHOP_ITEMS_QUERY -> {
+                if (p1 == null) return null
+                val id = p1.getAsInteger("id")
+                val name = p1.getAsString("name")
+                val amount = p1.getAsInteger("amount")
+                val enabled = p1.getAsBoolean("enabled")
+                val shopItem = ShopItem(
+                    id = id,
+                    name = name,
+                    amount = amount,
+                    enabled = enabled
+                )
+                shopListDao.addShopItemSync(mapper.mapEntityToDbModel(shopItem))
+            }
+        }
+        return null
+    }
+
+    override fun delete(p0: Uri, p1: String?, p2: Array<out String>?): Int {
+        when(uriMatcher.match(p0)) {
+            GET_SHOP_ITEMS_QUERY -> {
+                val id = p2?.get(0)?.toInt() ?: -1
+                return shopListDao.deleteShopItemSync(id)
+            }
+        }
+        return 0
+    }
+
+    override fun update(p0: Uri, p1: ContentValues?, p2: String?, p3: Array<out String>?): Int {
+        when(uriMatcher.match(p0)) {
+            GET_SHOP_ITEMS_QUERY -> {
+                if (p1 == null) return 0
+                val id = p1.getAsInteger("id")
+                val name = p3?.get(0) ?: p1.getAsString("name")
+                val amount = p3?.get(1)?.toInt() ?: p1.getAsInteger("amount")
+                val enabled = p1.getAsBoolean("enabled")
+                val shopItem = ShopItem(
+                    id = id,
+                    name = name,
+                    amount = amount,
+                    enabled = enabled
+                )
+                shopListDao.addShopItemSync(mapper.mapEntityToDbModel(shopItem))
+            }
+        }
+        return 0
+    }
+
+    companion object {
+        private const val GET_SHOP_ITEMS_QUERY = 100
+        private const val GET_SHOP_ITEM_BY_ID_QUERY = 101
+    }
+}
--- app/src/main/java/com/example/shoppinglist/presentation/ShopItemFragment.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopItemFragment.kt
@@ -1,6 +1,8 @@
 package com.example.shoppinglist.presentation
 
+import android.content.ContentValues
 import android.content.Context
+import android.net.Uri
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -13,6 +15,7 @@ import com.example.shoppinglist.R
 import com.example.shoppinglist.databinding.FragmentShopItemBinding
 import com.example.shoppinglist.domain.ShopItem
 import javax.inject.Inject
+import kotlin.concurrent.thread
 
 class ShopItemFragment : Fragment() {
 
@@ -109,19 +112,46 @@ class ShopItemFragment : Fragment() {
     private fun launchEditMode() {
         viewModel.getShopItem(shopItemId)
         binding.saveButton.setOnClickListener {
-            viewModel.editShopItem(
-                binding.etName.text?.toString(),
-                binding.etAmount.text?.toString()
-            )
+//            viewModel.editShopItem(
+//                binding.etName.text?.toString(),
+//                binding.etAmount.text?.toString()
+//            )
+            thread{
+                context?.contentResolver?.update(
+                    Uri.parse("content://com.example.shoppinglist/shop_items"),
+                    ContentValues().apply {
+                        put("id", viewModel.shopItem.value?.id)
+                        put("name", viewModel.shopItem.value?.name)
+                        put("amount", viewModel.shopItem.value?.amount)
+                        put("enabled", viewModel.shopItem.value?.enabled)
+                    },
+                    null,
+                    arrayOf(
+                        binding.etName.text?.toString(),
+                        binding.etAmount.text?.toString()
+                    )
+                )
+            }
         }
     }
 
     private fun launchAddMode() {
         binding.saveButton.setOnClickListener {
-            viewModel.addShopItem(
-                binding.etName.text?.toString(),
-                binding.etAmount.text?.toString()
-            )
+//            viewModel.addShopItem(
+//                binding.etName.text?.toString(),
+//                binding.etAmount.text?.toString()
+//            )
+            thread{
+                context?.contentResolver?.insert(
+                    Uri.parse("content://com.example.shoppinglist/shop_items"),
+                    ContentValues().apply {
+                        put("id", 0)
+                        put("name", binding.etName.text?.toString())
+                        put("amount", binding.etAmount.text?.toString())
+                        put("enabled", true)
+                    }
+                )
+            }
         }
     }
 
